9701f7f2816ca821aa383580f41550408eb59458
sampayo123 <israel_sampayo@hotmail.com>
2022-05-08 23:40:04 -0400

    Add project files.

---
+++ Moneda2/Server/Controllers/MonedasController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using static Moneda2.Client.Pages.Moneda;
+using Moneda2.Shared.Models;
+
+namespace Moneda2.Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MonedasController : Controller
+    {
+        public CultureInfo[] cultures = CultureInfo.GetCultures(CultureTypes.SpecificCultures);
+        public List<RegionInfo> countries = new List<RegionInfo>();
+        public List<RegionInfo> ListaRegion = new List<RegionInfo>();
+        public List<DatosMoneda> ListaDatosMoneda = new List<DatosMoneda>();
+        string MonedaAnterior;
+        [HttpGet]
+        public async Task<ActionResult<List<DatosMoneda>>> GetOportunidad()
+        {
+            foreach (CultureInfo ci in cultures)
+            {
+                RegionInfo regionInfo = new RegionInfo(ci.TextInfo.CultureName);
+                //RegionInfo regionInfo = new RegionInfo("es-ES");
+                if (countries.Count(x => x.CurrencyEnglishName == regionInfo.CurrencyNativeName) <= 0)
+                {
+                    if (MonedaAnterior != regionInfo.CurrencyNativeName)
+                    {
+                        MonedaAnterior =  regionInfo.CurrencyNativeName;
+                        
+                        countries.Add(regionInfo);
+
+                    }
+
+                }
+                    
+            }
+            foreach (RegionInfo regionInfo in countries.OrderBy(x => x.Name))
+            {
+
+            
+                ListaRegion.Add(regionInfo);
+            }
+            //Serializacion de los datos necesaria para region info
+            var jsonString = JsonSerializer.Serialize(ListaRegion);
+            ListaDatosMoneda = JsonSerializer.Deserialize<List<DatosMoneda>>(jsonString);
+
+            return ListaDatosMoneda;
+        }
+    }
+
+
+
+}
---
+++ Moneda2/Shared/Models/DatosMoneda.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Moneda2.Shared.Models
+{
+    public class DatosMoneda
+    {
+        public string Name { get; set; }
+        public string DisplayName { get; set; }
+        public string EnglishName { get; set; }
+        public bool IsMetric { get; set; }
+        public string ThreeLetterISORegionName { get; set; }
+        public string ThreeLetterWindowsRegionName { get; set; }
+        public string TwoLetterISORegionName { get; set; }
+        public string CurrencySymbol { get; set; }
+        public string ISOCurrencySymbol { get; set; }
+        public string CurrencyNativeName { get; set; }
+        public string CurrencyEnglishName { get; set; }
+        public string nativeName { get; set; }
+        public string geoId { get; set; }
+
+    }
+}
